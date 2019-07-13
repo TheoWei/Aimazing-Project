3b9761b7913b169aa68eda0181075a86679d6bb2
Theo Wei <topslimer@gmail.com>
2019-07-13 21:31:00 +0800

    update lodash

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const config = require('../config/config');
-
+console.log(config);
 module.exports = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
   dialect: config.dialect
